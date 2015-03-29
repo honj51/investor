b4f99cc37c37b32e744f4a20c633f6db8cc52faf
timest <timest.lyy@gmail.com>
2015-03-29 15:52:40 +0800

    完成13个平台的爬虫

---
+++ octopus/octopus/settings.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for octopus project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'octopus'
+
+SPIDER_MODULES = ['octopus.spiders']
+NEWSPIDER_MODULE = 'octopus.spiders'
+
+
+# USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36 SE 2.X MetaSr 1.0'
+USER_AGENT = 'Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)'
+
+
---
+++ octopus/octopus/spiders/p2p.py
@@ -0,0 +1,344 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+
+from octopus.items import Item
+
+def out(item):
+        print '---------%s------' % item['platform']
+        print u'标题: %s' % item['name']
+        print u'年利率: %s' % item['apr']
+        print u'期限: %s' % item['day']
+        print u'进度: %s' % item['progress']
+        print 
+
+"""
+1、温商贷
+2、温州贷
+3、紫枫信贷
+4、招商贷
+5、爱投资
+6、诺诺磅客
+7、翼龙贷 (Pause)
+8、四达投资
+9、中宝贷
+10、资本在线
+11、一起好
+12、渝都贷
+13、
+"""
+
+class Wsloan(scrapy.Spider):
+    c_name = u'温商贷'
+    name = 'wsloan'
+
+    start_urls = (
+        'https://www.wsloan.com/index.aspx',
+    )
+
+    def parse(self, res):
+        #转贷宝
+        node1 = res.xpath('/html/body/div[9]/div/div[2]/div[2]/div/ul') 
+        for i in node1:
+            item = Item(
+                name = i.xpath('li[1]/h3/a/text()').extract()[0].strip(),
+                apr = i.xpath('li[2]/span/text()').extract()[0].strip(),
+                day = i.xpath('string(li[4])').extract()[0].strip(),
+                progress = i.xpath('li[5]/font/text()').extract()[0].strip(),
+                platform = self.c_name
+            )
+            out(item)
+
+        #温商宝
+        node2 = res.xpath('/html/body/div[9]/div/div[3]/div[2]/div/ul') 
+        for i in node2:
+            item = Item(
+                name = i.xpath('li[1]/h3/a/text()').extract()[0].strip(),
+                apr = i.xpath('li[2]/span/text()').extract()[0].strip(),
+                day = i.xpath('string(li[4])').extract()[0].strip(),
+                progress = i.xpath('li[5]/font/text()').extract()[0].strip(),
+                platform = self.c_name
+            )
+            out(item)
+
+class Wzdai(scrapy.Spider):
+    c_name = u'温州贷'
+    name = 'wzdai'
+
+    start_urls = ('https://www.wzdai.com', )
+
+    def parse(self, res):
+        #短期宝
+        node1 = res.xpath('/html/body/div[4]/div/div[1]/div[1]/ul')
+        for i in node1:
+            item = Item(
+                name = i.xpath('p/a/text()').extract()[0].strip(),
+                apr = i.xpath('string(li/span[3]/font)').extract()[0].strip(),
+                day = i.xpath('string(li/span[2]/font)').extract()[0].strip(),
+                progress = i.xpath('string(li/span[4]/div/font)').extract()[0].strip(),
+                platform = self.c_name
+            )
+            out(item)
+
+        #股盈宝专区
+        node2 = res.xpath('/html/body/div[4]/div/div[1]/div[2]/ul')
+        for i in node2:
+            item = Item(
+                name = i.xpath('p/a/text()').extract()[0].strip(),
+                apr = i.xpath('string(li/span[3]/font)').extract()[0].strip(),
+                day = i.xpath('string(li/span[2]/font)').extract()[0].strip(),
+                progress = i.xpath('string(li/span[4]/div/font)').extract()[0].strip(),
+                platform = self.c_name
+            )
+            out(item)
+
+        #车宝宝
+        node3 = res.xpath('/html/body/div[4]/div/div[1]/div[3]/ul')
+        for i in node3:
+            item = Item(
+                name = i.xpath('p/a/text()').extract()[0].strip(),
+                apr = i.xpath('string(li/span[3]/font)').extract()[0].strip(),
+                day = i.xpath('string(li/span[2]/font)').extract()[0].strip(),
+                progress = i.xpath('string(li/span[4]/div/font)').extract()[0].strip(),
+                platform = self.c_name
+            )
+            out(item)
+
+        #借款标列表
+        node4 = res.xpath('/html/body/div[4]/div/div[1]/div[4]/ul')
+        for i in node4:
+            item = Item(
+                name = i.xpath('p/a/text()').extract()[0].strip(),
+                apr = i.xpath('string(li/span[3]/font)').extract()[0].strip(),
+                day = i.xpath('string(li/span[2]/font)').extract()[0].strip(),
+                progress = i.xpath('string(li/span[4]/div/font)').extract()[0].strip(),
+                platform = self.c_name
+            )
+            out(item)
+
+class Zfxindai(scrapy.Spider):
+    c_name = u'紫枫信贷'
+
+    name = 'zfxindai'
+
+    start_urls = ('http://www.zfxindai.cn', )
+
+    def parse(self, res):
+        node = res.xpath('/html/body/div[6]/div[2]/div[1]/div[1]/div[2]/div[2]/div[3]/ul/li')
+        menu = 1
+        for i in node:
+            if menu: 
+                menu = 0
+                continue
+            item = Item(
+                name = i.xpath('span[1]/a/text()').extract()[0].strip(),
+                apr = i.xpath('string(span[3])').extract()[0].strip(),
+                day = i.xpath('string(span[4])').extract()[0].strip(),
+                progress = i.xpath('span[5]/span[2]/text()').extract()[0].strip(),
+                platform = self.c_name
+            )
+
+            out(item)
+
+class Zhaoshangdai(scrapy.Spider):
+    c_name = u'招商贷'
+
+    name = 'zhaoshangdai'
+
+    start_urls = ('http://www.zhaoshangdai.com/', )
+
+    def parse(self, res):
+        node = res.xpath('/html/body/div[3]/div[1]/div[1]/div[1]/div[2]/div')
+        menu = 1
+        for i in node:
+            if menu: 
+                menu = 0
+                continue
+            item = Item(
+                name = i.xpath('ul/li[2]/span/a/text()').extract()[0].strip(),
+                apr = i.xpath('ul/li[4]/text()').extract()[0].strip(),
+                day = i.xpath('string(ul/li[5])').extract()[0].strip(),
+                progress = i.xpath('ul/li[7]/div[3]/text()').extract()[0].strip(),
+                platform = self.c_name
+            )
+
+            out(item)
+
+class Itouzi(scrapy.Spider):
+    """ 通过 GET ，获取标的JSON格式数据 """
+    c_name = u'爱投资'
+
+    name = 'itouzi'
+
+    start_urls = ('http://www.itouzi.com/dinvest/ajax/list?type=2', )
+
+    def parse(self, res):
+        node = json.loads(res.body)
+        for i in node['data']['borrows']:
+            item = Item(
+                name = i['name'],
+                apr = i['apr'],
+                day = i['timeLimit'],
+                progress = i['scale'],
+                platform = self.c_name
+            )
+            out(item)
+
+class Nonobank(scrapy.Spider):
+    """ 通过 POST ，获取标的JSON格式数据 """
+    c_name = u'诺诺磅客'
+
+    name = 'nonobank'
+
+    start_urls = ('https://www.nonobank.com/Licai/FinancePlanList', )
+
+    def parse(self, res):
+        return scrapy.Request(url='https://www.nonobank.com/Licai/GetLicaiList/8/1/', method='POST', callback=self.deal_list)
+
+    def deal_list(self, res):
+        node = json.loads(res.body)
+        for i in node['members']:
+            item = Item(
+                name = i['fp_title'],
+                apr = '%s ~ %s' % (i['fp_rate_min'], i['fp_rate_max']) ,
+                day = i['fp_expect'],
+                progress = i['fp_percent'],
+                platform = self.c_name
+            )
+            out(item)
+
+class Eloancn(scrapy.Spider):
+    """ 爬虫暂停， 因为自动投标导致没有机会购买标 """
+    c_name = u'翼龙贷'
+    name = 'eloancn'
+
+    start_urls = ('http://www.eloancn.com/new/loadAllTender.action', )
+
+    def parse(self, res):
+        #转贷宝
+        node = res.xpath('/html/body/div[7]/div[2]/div[4]/dl')
+        sign = 0
+        for i in node:
+            item = Item(
+                name = i.xpath('dd[%s]/h3/a/text()' % str(sign + 1)).extract()[0].strip(),
+                apr = i.xpath('dd[%s]/p[2]/span/text()' % str(sign + 2)).extract()[0].strip(),
+                day = i.xpath('string(dd[%s]/p[1]/span)' % str(sign + 3)).extract()[0].strip(),
+                progress = i.xpath('dd[%s]/p/span/em/text()' % str(sign + 4)).extract()[0].strip(),
+                platform = self.c_name
+            )
+            sign += 5
+            out(item)
+
+class Sidatz(scrapy.Spider):
+    c_name = u'四达投资'
+    name = 'sidatz'
+
+    start_urls = ('https://www.sidatz.com/', )
+
+    def parse(self, res):
+        #转贷宝
+        node = res.xpath('//*[@id="aspnetForm"]/section/div[6]/table[2]/tr')
+        menu = 0
+        for i in node:
+            if menu == 0 : 
+                menu  = 1 
+                continue
+            item = Item(
+                name = i.xpath('td[1]/a/text()').extract()[0].strip(),
+                apr = i.xpath('td[4]/text()').extract()[0].strip(),
+                day = i.xpath('td[5]/b/text()').extract()[0].strip(),
+                progress = i.xpath('string(td[6]/span)').extract()[0].strip(),
+                platform = self.c_name
+            )
+            out(item)
+
+class Zhongbaodai(scrapy.Spider):
+    c_name = u'中宝贷'
+    name = 'zhongbaodai'
+
+    start_urls = ('http://www.zhongbaodai.com/#!/invest', )
+
+    def parse(self, res):
+        return scrapy.Request(url='http://www.zhongbaodai.com/service/lending/public_project?limit=10&sort=status+asc%7Cid+desc',  method='GET', callback=self.deal_list)
+
+    def deal_list(self, res):
+        node = json.loads(res.body)
+        for i in node:
+            item = Item(
+                name = i['controls'],
+                apr = '%s%%' % (float(i['rate']) * 12) ,
+                day = u'%s个月' % i['periods'] ,
+                progress = float(i['progress']) * 100,
+                platform = self.c_name
+            )
+
+            out(item)
+        
+class Zibenzaixian(scrapy.Spider):
+    c_name = u'资本在线'
+    name = 'zibenzaixian'
+
+    start_urls = ('http://www.zibenzaixian.com/jd/want_invest/forwardInvest/borrowing.jd', )
+
+    def parse(self, res):
+        node = res.xpath('//*[@id="Tab3"]/div/div[1]/table/tbody')
+        for i in node:
+            item = Item(
+                name = i.xpath('tr/td[1]/div/a/span[1]/text()').extract()[0],
+                apr = i.xpath('tr/td[4]/text()').extract()[0].strip(),
+                day = i.xpath('tr/td[6]/text()').extract()[0].strip(),
+                progress = i.xpath('tr/td[7]/span/i[1]/text()').extract()[0].strip(),
+                platform = self.c_name
+            )
+            out(item)
+
+class Yiqihao(scrapy.Spider):
+    c_name = u'一起好'
+    name = 'yiqihao'
+
+    start_urls = ('https://www.yiqihao.com/loan/list', )
+
+    def parse(self, res):
+        return scrapy.Request('https://www.yiqihao.com/loan/list', method='POST', callback=self.deal_list)
+
+    def deal_list(self, res):
+        node = json.loads(res.body)
+        for i in node['data']['list']:
+            item = Item(
+                name = i['title'],
+                apr = i['apr'],
+                day = '%s%s' % (i['deadline'], i['deadline_type']),
+                progress = '%s%%' % ( (float(i['remain_amount']) / float(i['amount'])) * 100 ),
+                platform = self.c_name
+            )
+            out(item)
+
+class Yududai(scrapy.Spider):
+    c_name = u'渝都贷'
+    name = 'yududai'
+
+    start_urls = ('http://www.yududai.com/invest/index.html', )
+
+
+    def parse(self, res):
+        node = res.xpath('//div[re:test(@class, "invest-box-list")]')
+
+        for i in node:
+            item = Item(
+                name = i.xpath('ul/li/span[1]/a[1]/text()').extract()[0].strip(),
+                apr = i.xpath('ul/li/span[3]/text()').extract()[0].strip(),
+                day = u'%s个月' % (i.xpath('string(ul/li/span[4]/strong)').extract()[0]),
+                progress = i.xpath('ul/li/span[6]/b/text()').extract()[0].strip(),
+                platform = self.c_name
+            )
+            out(item)
+
+
+
+class Gongshidai(scrapy.Spider):
+    name = 'gongshidai'
+
+    start_urls = ('http://192.168.1.167:81', )
+
+    def parse(self, res):
+        print res.body
